9b2f862d1860e676a4e3e7db72fe044590c61244
alcyonebots <identicate.in@gmail.com>
2025-07-29 18:44:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+API_ID = 29872536
+API_HASH = "65e1f714a47c0879734553dc460e98d6"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# Telegram user IDs allowed to control the bot
+ADMIN_IDS = [123456789, 987654321]
+
+# MongoDB
+MONGO_URI = "mongodb+srv://username:password@cluster.mongodb.net/"
+MONGO_DB = "idnunu"
+MONGO_COLLECTION = "sessions"
