1cf7e8ea1221f77785e4e5e68297d5f08bfc1e6f
alcyonebots <identicate.in@gmail.com>
2025-07-30 09:31:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_HASH = "65e1f714a47c0879734553dc460e98d6"
 BOT_TOKEN = "7527053101:AAFWX7cAgH48AlwG60Kozqrr5A7rdEL-Ezw"
 
 # Telegram user IDs allowed to control the bot
-ADMIN_IDS = [6663845789, 8034717776, 1110013191]
+ADMIN_IDS = [6663845789]
 
 # MongoDB
 MONGO_URI = "mongodb+srv://botnet:botnet@cluster0.izjogcb.mongodb.net/"
